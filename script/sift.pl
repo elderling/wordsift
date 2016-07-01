efabf6dd0bc131f0bddf0e7da83c33ac26268f97
Jonathan W. Taylor <elderling@gmail.com>
2016-07-01 17:57:28 -0500

    perltidy

--- script/sift.pl
+++ script/sift.pl
@@ -13,20 +13,21 @@ use Dictionary;
 my $min_word_length;
 my $max_word_length;
 my $max_matches;
-my $available_letters; 
+my $available_letters;
 
-my $result = GetOptions( 
-    'min-word-length=i' => \$min_word_length,
-    'max-word-length=i' => \$max_word_length,
-    'max-matches=i' => \$max_matches,
-    'available-letters=s' => \$available_letters, );
+my $result = GetOptions(
+    'min-word-length=i'   => \$min_word_length,
+    'max-word-length=i'   => \$max_word_length,
+    'max-matches=i'       => \$max_matches,
+    'available-letters=s' => \$available_letters,
+);
 
 print $available_letters;
 exit if !$available_letters;
 
 $min_word_length ||= 7;
-$max_matches ||= 10;
-$max_word_length ||=100;
+$max_matches     ||= 10;
+$max_word_length ||= 100;
 
 print "Requested matches: $max_matches\n";
 print "Minimum word length: $min_word_length\n";
@@ -36,7 +37,7 @@ print "Available letters: $available_letters\n";
 my $dictionary = Dictionary->new()->word_list();
 
 my @filtered =
-  grep { (length $_ >= $min_word_length and length $_ <= $max_word_length) }
+  grep { length $_ >= $min_word_length && length $_ <= $max_word_length }
   @{$dictionary};
 
 chomp(@filtered);
@@ -50,7 +51,7 @@ my @matches;
 foreach my $word (@filtered) {
     if ( WordSift::match( $word, $available_letters ) ) {
         push @matches, $word;
-        if ( scalar @matches == $max_matches ) { last; };
+        if ( scalar @matches == $max_matches ) { last; }
     }
 }
 
