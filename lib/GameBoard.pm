127f71d4a34edae2b3f9c168fff2f22404163322
Jonathan W. Taylor <elderling@gmail.com>
2016-07-02 20:41:44 -0500

    Swapped x & y so that it will be easier te shift letters down once other letters are removed

--- lib/GameBoard.pm
+++ lib/GameBoard.pm
@@ -8,32 +8,33 @@ use warnings;
 
 # 4 letters 5 letters
 my $board_3x3_a = [ 
-    [qw( b k o )], 
-    [qw( e o o )], 
-    [qw( l b w )], 
+    [qw( b e l )], 
+    [qw( k o b )], 
+    [qw( o o w )], 
 ];
 
 # 4 letters 6 letters 6 letters
 my $board_4x4_a = [
-    [qw( l a t o )],
-    [qw( o l t r )],
-    [qw( g e e a )],
-    [qw( b u g n )],
+    [qw( l o g b )],
+    [qw( a l e u )],
+    [qw( t t e g )],
+    [qw( o r a n )],
 ];
 
 # 4 letters 5 letters
 my $board_3x3_b = [
-    [qw( s k e )],
-    [qw( m t t )],
-    [qw( o e n )],
+    [qw( s m o )],
+    [qw( k t e )],
+    [qw( e t n )],
 ];
 
 # 4 letters 4 letters 8 letters
+
 my $board_4x4_b = [
-    [qw( f h f s )],
-    [qw( l s i k )],
-    [qw( a r c e )],
-    [qw( g a t n )],
+    [qw( f l a g )],
+    [qw( h s r a )],
+    [qw( f i c t )],
+    [qw( s k e n )],
 ];
 #>>>
 
@@ -49,7 +50,7 @@ sub spit_board {
 
     my $board = $board_4x4_b;
     for my $x ( 0 .. $#{$board} ) {
-        for my $y ( 0 .. $#{$board} ) {
+        for my $y ( 0 .. $#{ $board->[$x] } ) {
             say "x => $x , y => $y, value => " . $board->[$x][$y];
         }
     }
